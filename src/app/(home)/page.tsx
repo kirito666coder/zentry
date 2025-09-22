c97241bd9ee6279ad97905e0fd9a7a22ca3762c8
kirito <kirito666codr@gmail.com>
2025-09-22 15:49:40 +0530

    Remove unused SVG files and the main page component, and update global styles with new color and font variables.

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,10 @@
+import Hero from "./Hero";
+
+
+export default function page() {
+  return (
+    <div className="relative min-h-screen w-screen overflow-x-hidden" >
+      <Hero/>
+    </div>
+  )
+}
