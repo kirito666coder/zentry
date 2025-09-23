5e4b74c82bce98998faa8dee08ba2f73c6b06409
kirito <kirito666codr@gmail.com>
2025-09-23 23:05:33 +0530

    update Hero and page components for layout improvements.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,8 +3,8 @@ import Hero from "./Hero";
 
 export default function page() {
   return (
-    <div className="relative min-h-screen w-screen overflow-x-hidden" >
+    <main className="relative min-h-screen w-screen overflow-x-hidden" >
       <Hero/>
-    </div>
+    </main>
   )
 }
