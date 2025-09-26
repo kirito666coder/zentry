c19834a7a78fe4d8ce1826b318bbb5fba7df739d
kirito <kirito666codr@gmail.com>
2025-09-27 00:19:44 +0530

    Integrate AnimatedTital component into About section for enhanced title display and styling

--- src/app/(home)/About.tsx
+++ src/app/(home)/About.tsx
@@ -2,6 +2,7 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import Image from "next/image";
+import AnimatedTital from "../components/AnimatedTital";
 
 
 export default function About() {
@@ -30,9 +31,9 @@ export default function About() {
             <h2 className="font-general text-sm uppercase md:text-[10px]">
                Welcome to Zentry
             </h2>
-            <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
-               Disc<b>o</b>ver the world's <br/> l<b>a</b>rgest shared
-            </div>
+
+            <AnimatedTital title="Disc<b>o</b>ver the world's <br/> l<b>a</b>rgest shared adventure" className="mt-5 !text-black text-center"/>
+
             <div className="about-subtext">
                  <p>The Game of Games begins-your life, now an epic MMORPG</p>
                  <p>Zentry unites every player from countless games and platforms</p>
