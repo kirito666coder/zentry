6470e728f1051190871b1de4e4eb08fdeea61045
kirito <kirito666codr@gmail.com>
2025-10-01 17:03:05 +0530

    Add BentoCard component to Features section with video support and descriptions for enhanced product presentation.

--- src/app/(home)/Features.tsx
+++ src/app/(home)/Features.tsx
@@ -1,3 +1,36 @@
+import { ReactNode } from "react"
+import { TiLocationArrow } from "react-icons/ti"
+
+type BentoCardProps = {
+src:string,
+title:ReactNode,
+description:string,
+isComingSoon?:boolean
+}
+
+const BentoCard = ({src,title,description,isComingSoon}:BentoCardProps) =>{
+  return(
+    <div className="relative size-full ">
+      <video
+      src={src}
+      loop
+      muted
+      autoPlay
+      className="absolute left-0 top-0 size-full object-center object-cover"
+      />
+      <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
+      <div>
+     <h1 className="bento-title special-font">{title}</h1>
+     {
+       description &&(
+         <p className="mt-3 max-w-64 text-xs md:text-base">{description}</p>
+        )
+      }
+      </div>
+      </div>
+    </div>
+  )
+}
 
 export default function Features( ) {
   return (
@@ -11,15 +44,57 @@ export default function Features( ) {
                     Immerse yourself in in a rich and ever-expanding universe where a vibrant array of products converge into an interconnected overlay experience on you world.
                  </p>
             </div>
-        </div>
 
             <div className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
-                 {/* <BentoCard
+                 <BentoCard
                   src='videos/feature-1.mp4'
                   title={<>radi<b>n</b>t</>}
-                  description=''
-                 /> */}
+                  description='A cross-platform metagame app, turning your activities across  web2 and web3 games into a rewarding adventure.'
+                  isComingSoon
+                  />
             </div>
+
+            <div className="grid h-[135vh] w-full grid-cols-2 grid-rows-3 gap-7">
+              <div className="bento-tilt_1 row-span-1 md:!col-span-1 md:row-span-2">
+                <BentoCard
+                src="videos/feature-2.mp4"
+                title={<>zig<b>m</b>a</>}
+                description="An anime and gaming-inspired NFT collection - the IP primed for expansion."
+                />
+              </div>
+              <div className="bento-tilt_1 row-span-1 ml-32 md:!col-span-1 md:ml-0 ">
+                <BentoCard
+                src="videos/feature-3.mp4"
+                title={<>n<b>e</b>xus</>}
+                description="A gamified social hub, adding a new dimension of play to social interaction for web3 communities."
+                />
+              </div>
+              <div className="bento-tilt_1 mr-14 md:mr-0 md:!col-span-1">
+                <BentoCard
+                src="videos/feature-4.mp4"
+                title={<>az<b>u</b>l</>}
+                description="A cross-world AI Agent - elevating your gameplay to be more fun and productive."
+                />
+              </div>
+
+
+              <div className="bento-tilt_2">
+                <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
+                  <h1 className="bento-title special-font max-w-64 text-black">M<b>o</b>re Co<b>m</b>ing s<b>o</b>on</h1>
+                  <TiLocationArrow className="m-5 scale-[5] self-end"/>
+                </div>
+              </div>
+              <div className="bento-tilt_2">
+                <video
+                src={'videos/feature-5.mp4'}
+                loop
+                muted
+                autoPlay
+                className="size-full object-cover object-center"
+                />
+              </div>
+            </div>
+          </div>
     </section>
   )
 }
