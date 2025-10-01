e541d71aafbf9a665580c8a433c1fbaf3460ffe6
kirito <kirito666codr@gmail.com>
2025-10-01 18:02:42 +0530

    Remove BentoCard component implementation from Features.tsx, streamlining the code and relying on BentoTilt for feature presentation.

--- src/app/(home)/Features.tsx
+++ src/app/(home)/Features.tsx
@@ -1,38 +1,8 @@
-import { ReactNode} from "react"
 import { TiLocationArrow } from "react-icons/ti"
 import { BentoTilt } from "../components/Bento/BentoTilt"
+import { BentoCard } from "../components/Bento/BentoCard"
 
 
-type BentoCardProps = {
-src:string,
-title:ReactNode,
-description:string,
-isComingSoon?:boolean
-}
-
-const BentoCard = ({src,title,description,isComingSoon}:BentoCardProps) =>{
-  return(
-    <div className="relative size-full ">
-      <video
-      src={src}
-      loop
-      muted
-      autoPlay
-      className="absolute left-0 top-0 size-full object-center object-cover"
-      />
-      <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
-      <div>
-     <h1 className="bento-title special-font">{title}</h1>
-     {
-       description &&(
-         <p className="mt-3 max-w-64 text-xs md:text-base">{description}</p>
-        )
-      }
-      </div>
-      </div>
-    </div>
-  )
-}
 
 export default function Features( ) {
   return (
