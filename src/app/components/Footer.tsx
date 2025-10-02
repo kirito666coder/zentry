4da5500dfdaaa3a58d92b3f968debe0f0b2d999b
kirito <kirito666codr@gmail.com>
2025-10-03 00:07:20 +0530

    Revamp Footer component with social media links and privacy policy, enhancing layout and user engagement.

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,33 @@
+import Link from "next/link"
+import { FaDiscord, FaGithub, FaTwitch, FaTwitter } from "react-icons/fa"
+
+const Links = [
+    {href:'/',icon:<FaDiscord/>},
+    {href:'/',icon:<FaTwitter/>},
+    {href:'/',icon:<FaGithub/>},
+    {href:'/',icon:<FaTwitch/>},
+]
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="w-screen bg-violet-300 py-4 text-black ">
+        <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+            <p className=" text-center text-sm md:text-left">
+                &copy;Nova 2024. All rights reserved
+            </p>
+            <div className="flex justify-center gap-4 md:justify-start">
+              {
+                Links.map((link,i)=>(
+                 <Link href={link.href} key={i} className="text-black transition-colors duration-500 ease-in-out hover:text-white">
+                    {link.icon}
+                 </Link>
+                ))
+              }
+            </div>
+            <Link href={'/'} className="text-center text-sm hover:underline md:text-right">
+            Privacy Policy
+            </Link>
+        </div>
+    </footer>
   )
 }
