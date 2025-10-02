f672d0afdc96376a7e5aa2ee9f4e97cabd0dc834
kirito <kirito666codr@gmail.com>
2025-10-02 23:54:12 +0530

    Implement Contact component with image clips and a call-to-action button, enhancing the layout and visual appeal of the contact section.

--- src/app/(home)/Contact.tsx
+++ src/app/(home)/Contact.tsx
@@ -1,6 +1,54 @@
+import Image from "next/image";
+import Button from "../components/Button";
+
+
+type ImageClipBoxProps ={
+    src:string,
+    className:string,
+}
+
+const ImageClipBox = ({src,className}:ImageClipBoxProps) =>{
+    return(
+        <div className={className}>
+        <Image
+        src={src}
+        alt=""
+        height={2000}
+        width={2000}
+        />
+        </div>  
+    )
+}
+
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div id="contact" className="my-20 min-h-96 w-screen px-10">
+        <div className="relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden">
+            <div className="absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96">
+             <ImageClipBox src="/img/contact-1.webp" className="contact-clip-path-1"/>
+             <ImageClipBox src="/img/contact-2.webp" className="contact-clip-path-2 lg:translate-y-40 translate-y-60"/>
+            </div>
+            <div className="absolute -top-40 left-20 w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80">
+             <ImageClipBox src="/img/swordman-partial.webp" className="absolute md:scale-125"/>
+             <ImageClipBox src="/img/swordman.webp" className="sword-man-clip-path md:scale-125"/>
+            </div>
+
+            <div className="flex flex-col items-center text-center">
+                <p className="font-general text-[10px] uppercase hidden md:block">
+                    join zentry
+                </p>
+                <p className="special-font mt-10 w-full font-zentry text-5xl leading-0.9 md:text-[6rem]">
+                    Let&apos;s b<b>u</b>ild the<br/> new era of <br/> g<b>a</b>ming t<b>o</b>gether
+                </p>
+                <Button
+                id="contact-us"
+                title="contact us"
+                className="mt-10 cursor-pointer"
+                />
+
+            </div>
+        </div>
+    </div>
   )
 }
